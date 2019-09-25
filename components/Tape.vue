aec2badc27c13d16c92cafc07289063ee2af861c
ray lot <rlotmar@hotmail.com>
2019-09-25 23:21:01 +0200

    initial commit

---
+++ components/Tape.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <!-- <img class="image" src="~/assets/tape_mit_text_741.png"> -->
+    
+  </div>
+</template>
+
+<style scoped>
+
+
+
+image {
+  width:  20px !important; 
+  height: 30px !important;
+  size: 10% 10% ;
+  display: block;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateX(0deg);
+  }
+}
+
+@keyframes godown {
+  100% {
+    top: 180px;
+  }
+}
+
+@keyframes goright {
+  100% {
+    left: 70px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <section class="container">
+        <!-- <img src="'~/assets/tape_mit_text.png"> -->
+
+    <div >
+       <img class=tape src="~/assets/tape_mit_text_741.png">
+   
+       <!-- 
+      <h2 class="subtitle">
+        under construction asbest website
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >GitHub</a>
+      </div> -->
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Tape from '~/components/tape.vue'
+
+import ImageUrl from '~/assets/tape_mit_text_741.png'
+
+export default {
+ 
+
+  components: {
+    Tape
+  }
+  
+
+}
+</script>
+
+<style>
+
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  /* justify-content: right;
+  align-items: center;
+  text-align: center; */
+  padding-right: 0px;
+  margin-right: 0px;
+  padding-top: 422px;
+
+
+
+  
+}
+.tape{
+  justify-content: right;
+  align-items: flex-end;
+}
+
+
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>

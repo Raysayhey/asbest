a294268f7353ecf6e4c212cd2c3e8c9a34688e1f
Mikheil Nemsitsveridze <genuine.realman@outlook.com>
2019-10-04 18:56:46 +0300

    landscape

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
